2ea1141b1db3e92cf9ce1ca2a935ee0391fbe19c
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-25 10:05:23 -0400

    aula 3 concluida e aula 4 iniciada

---
+++ Modulo_basico/aula_03.py
@@ -0,0 +1,24 @@
+#str
+
+'''
+str são as strings, textos, caracteres de escrita.
+em python, ao usarmos as str, colocamos elas entre aspas duplas ("") ou aspas simples(''), sendo que
+se abrimos de uma forma, fechamos da mesma forma.
+Pelo python ser uma linguagem do tipo dinâmica e forte, não preciso fazer como em outras linguagens
+e deixar discriminado o tipo de valor que a variável vai receber.
+'''
+numbers = 14 #variáveis em python sempre são minusculas seguidas do = que significa atribuir
+#e como podem ver, só atribui ao nome number numeros, não escrevi em nenhum local que ele é do tipo
+#inteiro
+print(numbers)
+
+"""
+Caracter de escape. 
+Usamos o caracter de escape para exibir determinados simbolos no nosso código
+sem que o imperpretador do python os execute. ex
+"""
+print("Matheus \"Roks\"")
+#nesse caso, para as aspas não entrarem em conflito usei o separador. 
+#mas nesse caso apresentado tambem poderia ter utilizado de aspas simples e duplas, veja
+print('Matheus "Roks"')
+print("Matheus 'Roks'")
