cab6fc83ad482c9bc9eef4b2c39f380ecabdb296
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-25 11:47:23 -0400

    Create aula-05.py

---
+++ Modulo_basico/aula-05.py
@@ -0,0 +1,6 @@
+#dados boleanos -> boolean -> bool
+#boolean é um outro tipo de dado em python que checa se a comparação/informação
+# é True (verdadeira) ou False(falsa)
+
+ok = True #repare que o sinal de atribuição foi usado e o dado que foi atribuido está em letra maiuscula, sem aspas e etc. por isso recebe o tipo boolena
+print(type(ok))
