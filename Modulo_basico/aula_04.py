5e068f0276f40926bd4f7c7740b0be52bf3ca6ee
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-25 10:08:20 -0400

    Update aula_04.py

---
+++ Modulo_basico/aula_04.py
@@ -0,0 +1,6 @@
+'''
+int -> inteiro ->  numero sem casas decimais
+float -> ponto flutuante -> possui casas decimais
+aqui segue o mesmo principio matemático, se um numero não apresenta sinal, ele é
+positivo.
+'''
