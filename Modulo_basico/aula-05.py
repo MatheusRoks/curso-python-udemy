794e4c0033632e4006c313c47fd057ec1b4a4947
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-25 13:12:31 -0400

    Update aula-05.py

--- Modulo_basico/aula-05.py
+++ Modulo_basico/aula-05.py
@@ -26,5 +26,40 @@ Junto do tipo boolean, possui os operadores lógicos que são usados com grande 
 *=    :mesmo esquema da +=, mas com multiplicação
 /=    :mesmo do anterior mas com divisão
 %=    :mesmo do anterior mas com módulo
-
+vamos aos exemplos
 """
+number_1 = 1 #aqui foi exemplificado a atribuição
+number_2 = 1
+number_3 = 2
+number_4 = 20
+number_5 = 3
+
+print(number_1==number_2)#retorna true 
+print(number_1!=number_2)#retorna false 
+print(number_1+number_2)#retorna 2
+print(number_1-number_2)#retorna 0
+number_1+=2
+print(number_1)#retorna 3 pos esssa variável foi alterada nesse momento
+number_1-=2
+print(number_1)#aqui volta a exibir 1 pois eu alterei mais uma vez a variável
+print(number_1*number_5)#multiplicação
+print(number_3**number_5)#exponenciação
+print(number_4/number_3)#divisão que retorna float
+print(number_4//number_3)#divisão que retorna int
+print(number_4%number_3)#retorna o resto da divisão
+print(number_3>number_4)#faz a comparadação de maior que
+print(number_3>=number_4)#faz a comparadação de maior ou igual a
+print(number_3<number_4)#faz a comparadação de menor que
+print(number_3<=number_4)#faz a comparadação de menor ou igual
+print("-"*10)
+print("number_1 normal:",number_1)
+number_1*=2
+print("number_1 multipllicado por 2:",number_1)
+print("-"*10)
+print(number_1)
+number_1/=2
+print("number_1 dividido por 2:",number_1)
+print("-"*10)
+print(number_1)
+number_1%=2
+print("number_1 resto de uma divisão por 2:",number_1)
