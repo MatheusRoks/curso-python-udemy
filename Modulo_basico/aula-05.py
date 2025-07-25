ac56cc6bd97c276927ac7d3dc824c72e52ea8423
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-25 11:57:48 -0400

    Update aula-05.py

--- Modulo_basico/aula-05.py
+++ Modulo_basico/aula-05.py
@@ -4,3 +4,27 @@
 
 ok = True #repare que o sinal de atribuição foi usado e o dado que foi atribuido está em letra maiuscula, sem aspas e etc. por isso recebe o tipo boolena
 print(type(ok))
+
+"""
+Junto do tipo boolean, possui os operadores lógicos que são usados com grande frequencia.
+=    : significa atribuição, dá valor a uma variável ou constante
+==    : comparação, verifica se os valores comparados são iguais
++    : soma de valores(numeros) ou concatenação(junção de str)
++=    : ex x=1|  x+=1 | x=2
+-    :subtração
+-=    :ex: x=3| x-=1 | x=2
+*    : multiplicação
+**    :exponenciação
+!=    :verifica se os valores comparados sãoo DIFERENTES e retorna true se forem
+/    :divisão que pode gerar numero float
+//    :divisão que só gera numero int
+%    :modulo, resto da divisão
+>    :maior que
+<    : menor que
+>=    : maior ou igual a
+<=    :menor ou igual a
+*=    :mesmo esquema da +=, mas com multiplicação
+/=    :mesmo do anterior mas com divisão
+%=    :mesmo do anterior mas com módulo
+
+"""
