c1be572460bfb705ff51f78038f9bc372ffc5cd6
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-25 11:42:07 -0400

    Update aula_04.py

--- Modulo_basico/aula_04.py
+++ Modulo_basico/aula_04.py
@@ -3,4 +3,23 @@ int -> inteiro ->  numero sem casas decimais
 float -> ponto flutuante -> possui casas decimais
 aqui segue o mesmo principio matemático, se um numero não apresenta sinal, ele é
 positivo.
+
+caso deseje verificar o dado, pode apelar para a classe type() veja os exemplos
 '''
+# int e positivo
+print(11)
+
+# int e negativo
+print(-11)
+
+# float e positivo
+print(15.6)
+
+# float e negativo
+print(-15.6)
+
+# checando type
+print(type(15))
+
+# checando type
+print(type(15.5))
