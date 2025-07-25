3dc559d83bc9dba706f4275c7cc83a0f8dc8685f
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-07-25 13:49:41 -0400

    Update aula_07.py

--- Modulo_basico/aula_07.py
+++ Modulo_basico/aula_07.py
@@ -1,4 +1,20 @@
 '''
-Variáveis, regras e etc
+Variáveis, regras e etc 
 
+estrutura:
+elas devem ter nomes claros, que representem o que fazem;
+não devem iniciar com letras maiusculas;
+sem acentuação;
+sem espaço;
+priorize por fazer suas variáveis e constantes em inglês, mas so por habito mesmo;
+evite usos de caracter, use apenas o -_ no nome
+sempre seja especifico, nada de 'n1', escreva, a preguiça de escrever é um erro.
+EU Matheus, ainda adicionaria como regra usar um comentário explicando o que é.
+e atenção ao que essa informação:
+POR CONVENÇÃO VARIÁVEIS QUE COMEÇAM COM LETRA MAIUSCULA SÃO NA VERDADE
+CONSTANTES, ou seja, são dados que não devem ser alterados. Como o python não
+fornece esse tipo de str, por convenção se adota isso. então se vc pegou um projeto
+com o cooleguinha e ele colocou 'Base_da_conta' ou "BASE_DA_CONTA" não altere esse
+dado, se vc alterar é capaz de estragar o código. e coleguinha, coloque um comentário
+diga "esse dado não deve ser alterado pois é isso isso isso".
 '''
