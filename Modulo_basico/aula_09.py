5bd092c980adb7290c490fe643f12589200d5a89
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-08-01 10:31:18 -0400

    Create aula_09.py

---
+++ Modulo_basico/aula_09.py
@@ -0,0 +1,9 @@
+# esse é mais um exercicio, onde o usuário fornece seu peso, altura, idade e nome, e é feito um 
+#calculo de imc.
+
+user={
+    'Nome':"",
+    'Idade':"",
+    'Altura':"",
+    "Peso":"",
+}
