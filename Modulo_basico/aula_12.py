46e6beb312f929ec9e86ffd8c8751d3c830d1560
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-08-02 15:12:50 -0400

    novo update

---
+++ Modulo_basico/aula_12.py
@@ -0,0 +1,6 @@
+#fatiamento
+#var[inicio:fim:passo]
+var = 'Hello World'
+print(var[::-1]) #inverte a str
+print(var[3:])#pega tudo do indice 3 em diante
+print(var[3::2])#pega tudo do 3 em diante de 2 em 2
