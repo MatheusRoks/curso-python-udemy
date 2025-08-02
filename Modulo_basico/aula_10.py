97e8121eaa5f8741da3390d0cb94d14ff078dae1
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-08-02 10:24:51 -0400

    Create aula_10.py

---
+++ Modulo_basico/aula_10.py
@@ -0,0 +1,54 @@
+#aqui nessa aula meu objetivo é anotar sobre as f-strings e o .format.
+#as f-strings possuem algumas coisas interessantes, uma delas é a possibilidade de
+#na hora de exibir um dado, fromatar de froma mais rapida e limpa.
+#supunhetamos que temos os seguintes dados.
+# name = antonio
+# age = 23
+# height = 185cm
+# caso desejemos usar o print, em python escreveremos da seguinte forma
+# print(name + ' tem ' + age +' anos e sua altura é ' + height) forma normal em varias linguagens
+# ou faremos assim print(name,'tem', age, 'anos e a sua altura é', height)
+# agora caso usemos a f-string
+# print(f'{name} tem {age} aos e sua altura é {heigt}')
+# alem disso, em parametros com muitas casas decimais, podemos usar :.(numero de casas)f para
+# limitar a exibição. vamos a um exemplo
+
+count = 10/3
+print(count)
+print(f'{count:.2f}')
+
+# e agora vamos ao format
+# o format é um método, ele tambem vai auxiliar na formatação dos dados
+# vamos aos exemplos e anotações
+# vejamos algumas variáveis
+a, b, c, d = 'um', 'dois', 'tres', 123.152 #sim, ao fazer isso, declaro varias variáveis em uma linha
+#vamos ao formato
+formatado = "x1={},x2={},x3={},x4={}".format(a,b,c,d)
+#vamos ao print
+print(formatado)
+#x1=um,x2=dois,x3=tres,x4=123.152 esse foi o resultado. pq?
+# primeiro definimos 4 variaveis, depois disso definimos uma variável chamada formato
+# nessa variável formato, definimos uma str(esses x1 e etc) E ATRIBUIMOS ESSAS CHAVES as str
+# apos isso usamos a função format e dizemos a ordem que os dados irão aparecer, vamos mudar a ordem
+# para ficar mais claro
+formatado = "x1={},x2={},x3={},x4={}".format(b,c,a,d)
+#vamos ao print
+print(formatado)
+# x1=dois,x2=tres,x3=um,x4=123.152 esse foi o resultado. mas não acaba por ai
+# podemos retirar esse x1 e etc e colocar ele dentro de uma str (uma tupla) e brincar um pouco mais
+ordem = "x1={},x2={},x3={},x4={}"
+formatado = ordem.format(b,c,a,d)
+# vamos ao print
+print(formatado)
+#mas não acabou, dentro das chaves podemos passar ainda os indices (sendo o que se encontra dentro
+#dos parentes do format a lista) e assim mudar a ordem de exibição direto na nossa tupla
+ordem = "x1={0},x2={2},x3={3},x4={1}"
+formatado = ordem.format(b,c,a,d)
+# vamos ao print
+print(formatado)
+#ainda podemos usar o :.2f dentro dos parentes e ainda podemos exibir varias vezes o mesmo. veja
+ordem = "x1={0},x1={0},x1={0},x1={0},x2={2},x3={3},x4={1}"
+formatado = ordem.format(b,c,a,d)
+# vamos ao print
+print(formatado)
+#x1=dois,x1=dois,x1=dois,x1=dois,x2=um,x3=123.152,x4=tres
