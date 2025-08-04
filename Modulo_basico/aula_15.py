07b0af3e84ccaf759bfebde6e3a0e183a0710a69
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-08-04 07:43:57 -0400

    Create aula_15.py

---
+++ Modulo_basico/aula_15.py
@@ -0,0 +1,20 @@
+import os
+
+''' limpeza do terminal'''
+def clear_terminal():
+    os.system('cls' if os.name == 'ntr' else 'clear')
+
+number = input('digite um número')
+resto = 0
+while True:
+    if number.isdigit():
+        number = int(number)
+        resto = number%2
+        if resto == 0:
+            print(f'O numero {number} é par')
+            break
+        else:
+            print(f'O numero {number} é impar')
+            break
+    else:
+        print('digite apenas números inteiros')
