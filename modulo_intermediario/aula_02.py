23aef496ed8579c7b30c0e98762cc4f56a2da517
Matheus Roks <matheusroksgomesrodrigues@gmail.com>
2025-08-13 12:50:38 -0400

    Update aula_02.py

--- modulo_intermediario/aula_02.py
+++ modulo_intermediario/aula_02.py
@@ -22,4 +22,23 @@ isso impede que a nossa função quebre caso a quantidade de argumentos seja dif
 claro que o exemplo acima é desnecessário, visto que a função nativa sum já faz isso.
 mas serve para exemplificar o uso do *args.
 
+
+mas caso você passe para a sua função uma tupla como arguento, o *args irá 
+empacotar essa tupla dentro de outra tupla, veja:
+
+'''
+
+def soma(*args):
+    print(args)
+
+soma(1, 2, 3, 4, 5)  # (1, 2, 3, 4, 5)
+soma((1, 2, 3, 4, 5))  # ((1, 2, 3, 4, 5),)
+
+'''
+note que no segundo caso, o resultado é uma tupla dentro de outra tupla.
+isso porque o *args empacota os argumentos passados para a função em uma tupla.
+para desempacotar uma tupla e passar seus valores como argumentos individuais,
+basta usar o * na chamada da função:
+
+enquanto isso, a função nativa sum já faz isso internamente, ou seja, ela já desempacota a tupla passada como argumento.
 '''
